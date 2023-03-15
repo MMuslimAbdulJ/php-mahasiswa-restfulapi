48959051a48d1d938e4510c18e9e2d9db037e241
Muhammad Muslim Abdul Jabbaar <mmuslimabdulj@gmail.com>
2023-03-15 19:50:33 +0700

    modify index.php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,25 @@
 <?php 
 require_once __DIR__ . './../vendor/autoload.php';
 use muslim\restfulapi\App\Router;
+use muslim\restfulapi\Config\Database;
 use muslim\restfulapi\Controller\MahasiswaController;
 
-Router::add('GET', '/', MahasiswaController::class, 'index', []);
-Router::add('GET', '/', MahasiswaController::class, 'index', []);
-Router::add('GET', '/', MahasiswaController::class, 'index', []);
-Router::add('GET', '/', MahasiswaController::class, 'index', []);
+// Database::getConnection('prod'); // turn off for unit-test, turn on for production
+
+// Create Mahasiswa
+Router::add('POST', '/api/mahasiswa', MahasiswaController::class, 'createMahasiswa', []);
+
+// Get Mahasiswa
+Router::add('GET', '/api/mahasiswa/([0-9]*)', MahasiswaController::class, 'getMahasiswa', []);
+
+// Update Mahasiswa
+Router::add('PUT', '/api/mahasiswa/([0-9]*)', MahasiswaController::class, 'updateMahasiswa', []);
+
+// Delete Mahasiswa
+Router::add('DELETE', '/api/mahasiswa/([0-9]*)', MahasiswaController::class, 'deleteMahasiswa', []);
+
+header('Content-type: application/json; charset=UTF-8');
 
 Router::run();
+
 ?>
